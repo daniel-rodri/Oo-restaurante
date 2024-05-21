f488fb7cad4611086fdfdf36a03b2495108e7e08
daniel-rodri <162440520+daniel-rodri@users.noreply.github.com>
2024-05-21 10:07:11 -0300

    Add files via upload

--- restaurante.py
+++ restaurante.py
@@ -2,19 +2,23 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self,nome,categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self.nome = nome.title()
+        self.categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
        return f'{self.nome} | {self.categoria}'
 
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{"nome do restaurante".ljust(25)} | {"categoria".ljust(25)} | {"status".ljust(25)}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante. ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante. ativo}')
 
-restaurante_praça = Restaurante('praça','gourmet')
-restaurante_pizza = Restaurante('pizza','italiana')
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐'
 
-Restaurante.listar_restaurantes()
+    def alternar_estado(self):
+        self._ativo =  not self._ativo
