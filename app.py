0fa6fb54740d3fb44cbb821f22974264c6b56028
daniel-rodri <162440520+daniel-rodri@users.noreply.github.com>
2024-05-21 10:07:41 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from restaurante import Restaurante
+
+restaurante_praça = Restaurante('praça','goumet')
+restaurante_mexicano = Restaurante('mexican food', 'mexicana')
+restaurante_japones = Restaurante('japa','japonesa')
+
+restaurante_mexicano.alteernar_estado()
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
